3c2af21e501c763ea03d67d08adfbb9486cc667c
Dvontre Coleman <dvontrec@gmail.com>
2019-03-13 12:06:59 -0400

    Create first test

---
+++ users/test/create_test.js
@@ -0,0 +1,11 @@
+// Tests user creation
+// imports assertion library installed with mocha
+const assert = require('assert');
+// Creates a test for user creation
+describe('Creating records', () => {
+  // runs the actual test
+  it('saves a user', () => {
+    // assertion / what should happen
+    assert(1 + 1 === 2);
+  });
+});
