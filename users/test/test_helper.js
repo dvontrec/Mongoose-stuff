2f276f9348cc0d68bdd96ab1b5a7b6aabed101d8
Dvontre Coleman <dvontrec@gmail.com>
2019-03-13 12:58:50 -0400

    Add mongoose conection before tests are run

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -3,17 +3,21 @@
 // imports mongoose library to the file
 const mongoose = require('mongoose');
 
-// connects to the database using new URLparser
-mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true });
-// when connection is made
-mongoose.connection
-  // when the connection works log good to go
-  .once('open', () => console.log('Good to go!'))
-  // on error print put the error
-  .on('error', err => {
-    console.warn('Error', err);
-  });
-
+// runs before test suite starts
+before(done => {
+  // connects to the database using new URLparser
+  mongoose.connect('mongodb://localhost/users_test', { useNewUrlParser: true });
+  // when connection is made
+  mongoose.connection
+    // when the connection works
+    .once('open', () => {
+      done();
+    })
+    // on error print put the error
+    .on('error', err => {
+      console.warn('Error', err);
+    });
+});
 // defines a testing hook
 // runs before each test
 beforeEach(done => {
