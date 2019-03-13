83a16e1a319e074380b7c46c49237d9de254d7e6
Dvontre Coleman <dvontrec@gmail.com>
2019-03-13 12:28:20 -0400

    remove mongo oauth, and set mocha to exit

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -1,11 +1,15 @@
 // Tests user creation
 // imports assertion library installed with mocha
 const assert = require('assert');
+const User = require('../src/user');
 // Creates a test for user creation
 describe('Creating records', () => {
   // runs the actual test
   it('saves a user', () => {
+    // creates a new user with the username "Joe"
+    const joe = new User({ name: 'Joe' });
+    // saves the instance of the user model
+    joe.save();
     // assertion / what should happen
-    assert(1 + 1 === 2);
   });
 });
