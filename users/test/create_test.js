77e893bd5dae737c78d39edd84827d5daf3020a7
Dvontre Coleman <dvontrec@gmail.com>
2019-03-13 12:51:22 -0400

    Update user creation test

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -5,11 +5,15 @@ const User = require('../src/user');
 // Creates a test for user creation
 describe('Creating records', () => {
   // runs the actual test
-  it('saves a user', () => {
+  it('saves a user', done => {
     // creates a new user with the username "Joe"
     const joe = new User({ name: 'Joe' });
     // saves the instance of the user model
-    joe.save();
+    joe.save().then(() => {
+      // has joe been saved successfully
+      assert(!joe.isNew); // joe should not be new
+      done();
+    });
     // assertion / what should happen
   });
 });
